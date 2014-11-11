22476bf8f4bdc6666461ea8a92f037fe4b75c66f
Santiago Paiva <paiva.santiago@gmail.com>
2014-11-10 20:40:24 -0500

    added go.js

---
+++ js/go.js
@@ -0,0 +1,12 @@
+// This script connects to the Go server 
+
+function playvideo()
+{
+	var video = document.getElementById("gbvideo");
+};
+
+
+// Request a connection to Go server
+
+
+// Get response. 
