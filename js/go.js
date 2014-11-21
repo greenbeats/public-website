41d20ebc311a49f30569aeb808d5d2497bee8832
Santiago Paiva <paiva.santiago@gmail.com>
2014-11-21 10:28:26 -0500

    commit

--- js/go.js
+++ js/go.js
@@ -5,7 +5,7 @@ var url = "http://54.69.71.254:8080/timeStamp/";
 function httpGet()
 {
     var xml_http = null;
-
+    
     xml_http = new XMLHttpRequest();
     xml_http.open( "GET", url, false );
     xml_http.send( null );
@@ -15,7 +15,7 @@ function httpGet()
 };
 
 
-function playVideo()
+function parseString(json)
 {
 	print 'hello'; 
 	
