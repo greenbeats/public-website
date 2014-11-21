388677eea8fcfffe54e1d42d78d7f61d570f4b18
Santiago Paiva <paiva.santiago@gmail.com>
2014-11-21 15:47:55 -0500

    refactored go.js

--- static/js/go.js
+++ static/js/go.js
@@ -1,13 +1,6 @@
-function getSeconds(json){
-
-	var time = json.time; 
-	var seconds = time.toFixed(2) / 1000000000
-
-  return (seconds % 10);
-
-};
 
 function getHttp(){
+
 		var xml_http = null;
 		var url = "http://54.69.71.254:8080/timestamp/";
 
@@ -15,13 +8,20 @@ function getHttp(){
 		xml_http.open( "GET", url, false );
 		xml_http.send( null );
 		
-		var json = JSON.parse(xml_http.responseText);
+		return JSON.parse(xml_http.responseText);
 		
-		seconds = getSeconds(json);
-		
-		console.log(seconds);
+};
 
-	};
+function getSeconds(){
+
+	json = getHttp(); 
+	var time = json.time; 
+	var seconds = time.toFixed(2) / 1000000000
+
+  console.log(seconds % 10)  
+  return (seconds % 10);
+
+};
 
 
 // TODO
@@ -39,4 +39,4 @@ function hearbeat()
 };
 
 
-window.onload = getHttp;
+window.onload = getSeconds;
