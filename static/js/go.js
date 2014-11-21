19dfb72dbc44c53cdca413093e921df75019595d
Santiago Paiva <paiva.santiago@gmail.com>
2014-11-21 17:08:28 -0500

    updated go.js

--- static/js/go.js
+++ static/js/go.js
@@ -1,10 +1,56 @@
+// Global Variables
+
+var videoTime;
+var userCount;
+
+
 function main(){
 
-	var time = getSeconds();
-	var latency = getLatency();
-	setPlayTime(time);
+	videoTime = 0;
+	while(true){ 
+		getStartTime();
+		setPlayTime();
+		setTimeout(30000);
+	}
+	//display(userCount);
 
 }
+
+function getStartTime(){
+
+	var delay = 0;
+
+	for(var i=0; i<3; i++){
+
+		delay += Request();
+
+	}
+
+	delay = delay / 3;
+	Request();
+
+	videoTime = (videoTime + delay) % 10000; 
+
+};
+
+
+function Request(){
+
+	//the current time
+	var startTime = new Date().getTime();
+
+	json = getHttp(); 
+
+	var now = new Date().getTime(); 
+
+	var delay = now - startTime;
+	
+	var videoTime = json.time; 
+	var userCount = json.count;
+
+	return delay;
+
+};
 
 function getHttp(){
 
@@ -31,11 +77,11 @@ function getSeconds(){
 };
 
 
-function setPlayTime(time){
+function setPlayTime(){
 
 	var vid = document.getElementById("gbvideo");
-	console.log(time);
-	vid.currentTime = time; 
+	console.log(videoTime);
+	vid.currentTime = videoTime; 
 
 };
 
@@ -43,7 +89,7 @@ function setPlayTime(time){
          
 function getLatency(){
 	
-	
+
 	var start = new Date().getTime();
 
 	for(var i=0; i<3; i++){
@@ -66,3 +112,49 @@ function hearbeat()
 
 
 window.onload = main;
+
+/*
+
+
+
+main(){
+	getStartTime()
+	setVideo(videoTime);
+	display(userCount);
+}
+
+getStartTime(){
+
+	delay = 0;
+
+	for( 1->3)
+	{
+
+		delay += request()
+		
+	}
+
+	delay = delay / 3;
+
+	request();
+	videoTime = (videoTime + delay)%10
+
+
+}
+
+
+request(){
+
+	//the current time
+	startTime
+	
+	json = httpReq()
+
+	delay = now - startTime
+	
+	videoTime = json.time 
+	userCount =  json.userCount;
+
+	return Delay
+}
+*/
