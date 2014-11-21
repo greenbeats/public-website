a7301370c64b74371bfc3adea4d14aff786faed7
Santiago Paiva <paiva.santiago@gmail.com>
2014-11-21 15:42:28 -0500

    implemented getHttp in go.js

--- static/js/go.js
+++ static/js/go.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
+function getSeconds(json){
 
-	function getHttp(){
+	var time = json.time; 
+	var seconds = time.toFixed(2) / 1000000000
+
+  return (seconds % 10);
+
+};
+
+function getHttp(){
 		var xml_http = null;
 		var url = "http://54.69.71.254:8080/timestamp/";
 
@@ -8,23 +15,28 @@ $(document).ready(function(){
 		xml_http.open( "GET", url, false );
 		xml_http.send( null );
 		
-		return json = JSON.parse(JSON.parse(xml_http.responseText));
+		var json = JSON.parse(xml_http.responseText);
+		
+		seconds = getSeconds(json);
+		
+		console.log(seconds);
+
 	};
 
 
-function getSeconds(json){
-
-  console.log(json);
-};
+// TODO
          
 function getLatency()
 {
 	return null;
 };
 
+// TODO
+
 function hearbeat()
 {
 	return null; 
 };
 
-});
+
+window.onload = getHttp;
